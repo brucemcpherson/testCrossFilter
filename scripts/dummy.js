d47380dfd1f15e263d43b09d0f5895bb28d609eb
Bruce Mcpherson <bruce@mcpher.com>
2018-10-30 16:44:42 +0000

    clean

--- scripts/dummy.js
+++
@@ -1,6 +0,0 @@
-
-// if you call a library that need an advanced service thats not referenced in your main script you have to provole an authorization dialog.
-function dummyFusion() {
-  FusionTables.Query.sql('dummy');
-  FusionTables.Column.remove();
-}
